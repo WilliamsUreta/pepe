00fb5703847da18c6dfebfcff00dc3c8f7aaf6cb
WilliamsUreta <163853944+WilliamsUreta@users.noreply.github.com>
2025-10-07 14:23:32 -0300

    Update Footer.jsx

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -3,7 +3,7 @@ import { CardFooter } from "react-bootstrap";
 function Footer() {
     return (
         <CardFooter className="text-center py-3 bg-dark text-white">
-            &copy; {new Date().getFullYear()} Mi Portafolio. Todos los derechos reservados.
+            &copy; {new Date().getFullYear()} Williams Ureta. Todos los derechos reservados.
         </CardFooter>
     );
 }
